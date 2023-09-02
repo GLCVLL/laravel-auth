c05096690f24c3c5b0737f983fa1814ff400e855
GLCVLL <g.ioele96@outlook.it>
2023-09-02 20:27:16 +0200

    added index, destroy, show

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -13,7 +13,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::orderBy('updated_at', 'DESC')->get();
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
@@ -37,7 +38,7 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        //
+        return view('admin.projects.show', compact('project'));
     }
 
     /**
@@ -61,6 +62,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return to_route('admin.projects.index')->with('type', 'succes')->with('message', 'Project successfully deleted');
     }
 }

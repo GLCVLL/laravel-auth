1e6d230194e180e9889f5f95b72dddab76a09784
GLCVLL <g.ioele96@outlook.it>
2023-09-05 16:32:35 +0200

    added validation

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
 
 class ProjectController extends Controller
 {
@@ -22,7 +23,9 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $project = new Project();
+
+        return view('admin.projects.create', compact('project'));
     }
 
     /**
@@ -30,6 +33,40 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate(
+            [
+                'title' => 'required|string|max:50|unique:projects',
+                'description' => 'required|string',
+                'cover_image' => 'nullable|url',
+                'start_date' => 'required|date',
+                'end_date' => 'nullable|date',
+                'category' => 'required|string',
+                'technologies' => 'required|string',
+                'project_url' => 'nullable|url',
+                'github_url' => 'nullable|url',
+                'client' => 'nullable|string',
+                'role' => 'required|string',
+                'additional_notes' => 'nullable|string',
+                'visibility' => 'required|boolean',
+            ],
+            [
+                'title.required' => 'The title field is mandatory.',
+                'title.max' => 'The title field cannot exceed 50 characters.',
+                'title.unique' => 'The title field must be unique.',
+                'description.required' => 'The description field is mandatory.',
+                'cover_image.url' => 'The cover image must be a valid URL.',
+                'start_date.required' => 'The start date field is mandatory.',
+                'start_date.date' => 'The start date must be a valid date.',
+                'end_date.date' => 'The end date must be a valid date.',
+                'category.required' => 'The category field is mandatory.',
+                'technologies.required' => 'The technologies field is mandatory.',
+                'project_url.url' => 'The project URL must be a valid URL.',
+                'github_url.url' => 'The GitHub URL must be a valid URL.',
+                'role.required' => 'The role field is mandatory.',
+                'visibility.required' => 'The visibility field is mandatory.',
+            ]
+        );
+
         $data = $request->all();
 
         $project = new Project();
@@ -62,6 +99,42 @@ class ProjectController extends Controller
      */
     public function update(Request $request, Project $project)
     {
+        $request->validate([
+            'title' => [
+                'required',
+                'string',
+                'max:50',
+                Rule::unique('projects')->ignore($project),
+            ],
+            'description' => 'required|string',
+            'cover_image' => 'nullable|url',
+            'start_date' => 'required|date',
+            'end_date' => 'nullable|date',
+            'category' => 'required|string',
+            'technologies' => 'required|string',
+            'project_url' => 'nullable|url',
+            'github_url' => 'nullable|url',
+            'client' => 'nullable|string',
+            'role' => 'required|string',
+            'additional_notes' => 'nullable|string',
+            'visibility' => 'required|boolean',
+        ], [
+            'title.required' => 'The title field is mandatory.',
+            'title.max' => 'The title field cannot exceed 50 characters.',
+            'title.unique' => 'The title field must be unique.',
+            'description.required' => 'The description field is mandatory.',
+            'cover_image.url' => 'The cover image must be a valid URL.',
+            'start_date.required' => 'The start date field is mandatory.',
+            'start_date.date' => 'The start date must be a valid date.',
+            'end_date.date' => 'The end date must be a valid date.',
+            'category.required' => 'The category field is mandatory.',
+            'technologies.required' => 'The technologies field is mandatory.',
+            'project_url.url' => 'The project URL must be a valid URL.',
+            'github_url.url' => 'The GitHub URL must be a valid URL.',
+            'role.required' => 'The role field is mandatory.',
+            'visibility.required' => 'The visibility field is mandatory.',
+        ]);
+
         $data = $request->all();
 
         $project->update($data);
